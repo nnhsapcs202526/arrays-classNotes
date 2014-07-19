0df88ede9f4e94d226d8bce48ab148d39a229af3
Geoff Schmit <gcschmit@gmail.com>
2014-07-19 15:59:25 -0500

    prepare starter code for Spring 2026

---
+++ ArrayNotes.java
@@ -0,0 +1,131 @@
+import java.util.GregorianCalendar;
+
+/**
+ * Class live-coding notes on arrays.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArrayNotes
+{
+    public static void createArrayOfEvens()
+    {
+        /*
+         * An array is an ordered collection of elements of the same type. The type
+         *      can be a primitive type (e.g., int) or a class (e.g., Turtle or String).
+         *      
+         *  An array variable is like an object variable in that it must be declared and
+         *      initialized.
+         *      
+         *  The number in the square brackets (i.e., []) specifies the number of elements
+         *      in the array. The number of elements in the array cannot be changed.
+         *      
+         *  All elements in the array are initialized to their default values
+         *      (e.g., 0, false, null).
+         *      
+         *  This code creates an array that contains 10 int elements.
+         */
+        
+        
+        /*
+         * Set the value of each element in the array to the first 10 positive even
+         *      integers.
+         *      
+         *  "length" is used to query the number of elements in the array
+         *  
+         *  Square brackets are used to reference a specific element in the array
+         *      based on its index. Indicies are zero based.
+         */
+        
+        
+        // print the reference to the array and the array elements
+        
+
+    }
+    
+    public static void createArrayOfOdds()
+    {
+        /*
+         * An array literal (a.k.a., initializer list) is a pair of curly brackets containing
+         *      comma-separated values. It can be used to initialize the array. The length
+         *      of the array is inferred based on the number of elements in the literal.
+         */
+        
+        /*
+         * ArrayIndexOutOfBoundsException
+         * 
+         *  Arrays have a fixed length once initialized. The index specified must refer
+         *      to a valid index. Otherwise, an ArrayIndexOutOfBoundsException is
+         *      generated.
+         */
+
+        /*
+         * Array References
+         * 
+         *  Variables of type array, contain a reference to the array stored in the
+         *      computer's memory.
+         *      
+         *  Assigning one array variable's value to another, copies the reference, not
+         *      the array's elements.
+         */
+
+        
+        /*
+         * Enhanced For Loops work with arrays.  No wrapper classes required.
+         */
+
+        
+        /*
+         * Limitations of Enhanced For Loops
+         * 
+         */
+
+    }
+    
+    public static void createArrayOfCalendars()
+    {
+        /*
+         * Create an array of 12 calendars, each calendar initialized to the start
+         *      of each month.
+         *      
+         *  When we create an array of references to objects, each element is initialized
+         *      to null. We have to explicitly create *new* objects and assign the
+         *      corresponding references to each element.
+         */
+        
+        /*
+         * At this point, every element in the array has a value of null.
+         */
+
+        
+        /*
+         * Create new calendar objects and assign their references to each element
+         *      in the array.
+         */
+
+        
+        /*
+         * An enhanced for loop cannot modify the values of the elements in the array
+         *      (e.g., references to calendar objects), but we can call mutator methods
+         *      which modify the properties of the referenced objects
+         *      (e.g., day of the month).
+         */
+
+    }
+    
+    public static int sumStringOfInts(String numbers, String delimiter)
+    {
+        int sum = 0;
+        /*
+         * arrays can be return values
+         * 
+         * split is a String method on the AP quick reference
+         * 
+         * Its purpose is to break a string into an array of substrings based 
+         *      on a specified delimiter (e.g., " ").
+         */
+        
+        
+        return sum;
+    }
+}
