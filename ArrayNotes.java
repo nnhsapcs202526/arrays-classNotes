f1c04da034e5eed24aaad1ba651ed16068f1f211
mrcallaghan <mcallaghan@naperville203.org>
2026-01-07 14:23:08 -0600

    Continued notes on arrays

--- ArrayNotes.java
+++ ArrayNotes.java
@@ -37,9 +37,19 @@ public class ArrayNotes
          *  Square brackets are used to reference a specific element in the array
          *      based on its index. Indicies are zero based.
          */
+        for(int i = 0; i < evens.length; i++)
+        {
+            evens[i] = (i + 1) * 2;
+        }
         
         
         // print the reference to the array and the array elements
+        System.out.println(evens);
+        
+        for(int i = 0; i < evens.length; i++)
+        {
+            System.out.println(i + ": " + evens[i]);
+        }
         
 
     }
@@ -50,7 +60,9 @@ public class ArrayNotes
          * An array literal (a.k.a., initializer list) is a pair of curly brackets containing
          *      comma-separated values. It can be used to initialize the array. The length
          *      of the array is inferred based on the number of elements in the literal.
+         *      
          */
+        int[] odds = new int[] {1,3,5,7,9,11,13,15,17,19};
         
         /*
          * ArrayIndexOutOfBoundsException
@@ -59,6 +71,10 @@ public class ArrayNotes
          *      to a valid index. Otherwise, an ArrayIndexOutOfBoundsException is
          *      generated.
          */
+        for(int i = 0; i <= odds.length; i++)
+        {
+            //System.out.println(odds[i]);
+        }
 
         /*
          * Array References
@@ -69,17 +85,34 @@ public class ArrayNotes
          *  Assigning one array variable's value to another, copies the reference, not
          *      the array's elements.
          */
+        int[] moreOdds = odds;
+        odds[2] = 6;
+        System.out.println(moreOdds[2]);  //prints 6
 
         
         /*
          * Enhanced For Loops work with arrays.  No wrapper classes required.
          */
-
+        for(int odd : odds)
+        {
+            System.out.println(odd);
+        }
         
         /*
          * Limitations of Enhanced For Loops
          * 
+         * Local variable is a copy of the value in the array.
+         * 
          */
+        for(int odd : odds)
+        {
+            odd++;
+        }
+        
+        for(int odd : odds)
+        {
+            System.out.println(odd);
+        }
 
     }
     
